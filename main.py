68c5ea28cd746110c50166f2561398650af00afd
David Brown <davidb@netflix.com>
2020-03-21 21:39:37 -0500

    lintable with tox

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import argparse
 import socket
 import struct
-import sys
+import click
 import json
 from influxdb import InfluxDBClient
 from influxdb.exceptions import InfluxDBClientError
 
 MCAST_GRP = '224.1.1.1'
 MCAST_PORT = 8087
+
+
 # todo configurable port
 
 class InfluxReceiver:
     def __init__(self):
         # todo config block
         self.host = '127.0.0.1'
-        self.port = '8085'
+        self.port = '8086'
         self.user = 'indigo'
         self.password = 'indigo'
         self.database = 'indigo'
         self.mcastport = str(MCAST_PORT)
-
+        self.sock = None
         self.connection = None
 
     def connect(self):
@@ -40,14 +42,6 @@ class InfluxReceiver:
             password=self.password,
             database=self.database)
 
-        # todo reset config - CLI flag? config dict? ALIENS?!
-        #if self.pluginPrefs.get('reset', False):
-        #    try:
-        #        print(u'dropping old')
-        #        self.connection.drop_database(self.database)
-        #    except:
-        #        pass
-
         print(u'Connecting...')
         self.connection.create_database(self.database)
         self.connection.switch_database(self.database)
@@ -57,11 +51,11 @@ class InfluxReceiver:
     def send(self, strwhat, measurement='device_changes'):
         what = json.loads(strwhat)[0]
 
-        json_body=[
+        json_body = [
             {
                 'measurement': what['measurement'],
-                'tags' : what['tags'],
-                'fields':  what['fields']
+                'tags': what['tags'],
+                'fields': what['fields']
             }
         ]
 
@@ -76,9 +70,7 @@ class InfluxReceiver:
                 self.connection.write_points(json_body)
                 unsent = False
             except InfluxDBClientError as e:
-                #print(str(e))
                 field = json.loads(e.content)['error'].split('"')[1]
-                #measurement = json.loads(e.content)['error'].split('"')[3]
                 retry = json.loads(e.content)['error'].split('"')[4].split()[7]
                 if retry == 'integer':
                     retry = 'int'
@@ -88,16 +80,15 @@ class InfluxReceiver:
                 # now we know to try to force this field to this type forever more
                 try:
                     newcode = '%s("%s")' % (retry, str(json_body[0]['fields'][field]))
-                    #print(newcode)
                     json_body[0]['fields'][field] = eval(newcode)
                 except ValueError:
                     pass
-                    #print('One of the columns just will not convert to its previous type. This means the database columns are just plain wrong.')
+                    # One of the columns just will not convert to its previous type. This means the database columns are just plain wrong.
             except ValueError:
                 print(u'Unable to force a field to the type in Influx - a partial record was still written')
             except Exception as e:
                 print("Error while trying to write:")
-                print(unicode(e))
+                print(e)
         if retrylimit == 0 and unsent:
             print(u'Unable to force all fields to the types in Influx - a partial record was still written')
 
@@ -107,33 +98,31 @@ class InfluxReceiver:
             while True:
                 data, addr = self.sock.recvfrom(10240)
                 self.send(data)
-                #if data == b'stop':
-                #    print('Client wants me to stop.')
-                #    break
-                #else:
-                #    print("From addr: '%s', msg: '%s'" % (addr[0], data))
         finally:
             # Close socket
             self.sock.close()
             print('Server stopped.')
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--multicastport', help='muticast port', default='8087')
-    parser.add_argument('-s', '--server', help='influxdb host', default='127.0.0.1')
-    parser.add_argument('-p', '--port', help='influxdb port', default='8085')
-    parser.add_argument('-U', '--user', help='influxdb user', default='indigo')
-    parser.add_argument('-P', '--password', help='influxdb password', default='indigo')
-    parser.add_argument('-d', '--database', help='influxdb database', default='indigo')
-    args, unknown = parser.parse_known_args()
 
+@click.command()
+@click.option("--influxdb-host", type=str, default="localhost", show_default=True, env="INHOST", help="The influxdb host to connect to.")
+@click.option("--influxdb-port", type=int, default=8086, show_default=True, env="INPORT", help="The influxdb port to connect to.")
+@click.option("--influxdb-user", type=int, default="indigo", show_default=True, env="INUSER", help="The influxdb user to connect as.")
+@click.option("--influxdb-pass", type=str, default="indigo", show_default=True, env="INPASS", help="The influxdb password.")
+@click.option("--influxdb-database", type=str, default="indigo", show_default=True, env="INDB", help="The influxdb database.")
+@click.option("--multicastport", "-m", type=int, default=8087, show_default=True, env="MCPORT", help="The multicast port.")
+def main(influxdb_host, influxdb_port, influxdb_user, influxdb_pass, influxdb_database, multicastport):
     ir = InfluxReceiver()
-    ir.host = args.server
-    ir.port = int(args.port)
-    ir.user = args.user
-    ir.password = args.password
-    ir.database = args.database
-    ir.mcastport = args.multicastport
+    ir.host = influxdb_host
+    ir.port = influxdb_port
+    ir.user = influxdb_user
+    ir.password = influxdb_pass
+    ir.database = influxdb_database
+    ir.mcastport = multicastport
 
     ir.connect()
     ir.run()
+
+
+if __name__ == "__main__":
+    main()
