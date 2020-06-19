6effe82b24deda75838e651651fd5a76c44c4767
David Brown <davidb@netflix.com>
2020-06-18 23:41:49 -0500

    serve the other thing too

--- serve.py
+++ serve.py
@@ -6,6 +6,8 @@ from typing import List
 
 import json
 
+from indigo_protobuf.indigo_influx_pb2 import InfluxEvent
+from indigo_protobuf.indigo_influx_pb2_grpc import InfluxTranslatorServicer
 from indigo_protobuf.indigo_pb2 import IndigoUnknownMessage
 from indigo_protobuf.indigo_pb2_grpc import TranslatorServicer
 from indigo_protobuf.indigo_influx_outbound import make_unknown_message, InfluxOutbound
@@ -22,7 +24,11 @@ def get_messages_from_msg(strwhat) -> List[IndigoUnknownMessage]:
     return messages
 
 
-class InfluxReceiver(TranslatorServicer):
+def get_influx_events(source_list: List[IndigoUnknownMessage]) -> List[InfluxEvent]:
+    return [elt.event for elt in map(InfluxOutbound, source_list) if elt.sendable()]
+
+
+class IndigoReceiver(TranslatorServicer):
     """Just take messages off local multicast, cast them, and forward them on."""
 
     def Subscribe(self, request, context):  # -> InfluxUnknownMessage
@@ -51,19 +57,52 @@ class InfluxReceiver(TranslatorServicer):
             sock.close()
             print('Server stopped.')
 
+
+class InfluxReceiver(InfluxTranslatorServicer):
+    def InfluxSubscribe(self, request, context):  # -> InfluxEvent
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        print(f'Starting listening socket on port {str(request.multicast_port)}')
+        sock.bind(('', int(request.multicast_port)))
+        mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+        try:
+            # Receive "string containing JSON (which is a list of object)" messages
+            while context.is_active():
+                data, addr = sock.recvfrom(10240)
+                if data == b'stop':
+                    print('Client wants me to stop.')
+                    return
+                else:
+                    messages: List[IndigoUnknownMessage] = get_messages_from_msg(data)
+                    tosend: List[InfluxEvent] = get_influx_events(messages)
+                    for msg in tosend:
+                        print(msg)
+                        yield msg
+
+        finally:
+            # Close socket
+            sock.close()
+            print('Server stopped.')
+
 # server.py
 import grpc
 from indigo_protobuf import indigo_pb2_grpc
+from indigo_protobuf import indigo_influx_pb2_grpc
 
 
 def serve():
-    server = grpc.server(ThreadPoolExecutor(max_workers=1))
-    indigo_pb2_grpc.add_TranslatorServicer_to_server(InfluxReceiver(), server)
-
     server_address = f'{SERVER_IP_BINDING}:{SERVER_PORT}'
     print(f"My server address {server_address}")
+
+    server = grpc.server(ThreadPoolExecutor(max_workers=1))
+    indigo_pb2_grpc.add_TranslatorServicer_to_server(IndigoReceiver(), server)
+    indigo_influx_pb2_grpc.add_InfluxTranslatorServicer_to_server(InfluxReceiver(), server)
+
     server.add_insecure_port(server_address)
     server.start()
+
     try:
         while True:
             time.sleep(5)
